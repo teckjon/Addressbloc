42ce78bcdb5d22aeecadbbee253a7a7777261254
teckjon <demospata@hotmail.com>
2015-10-12 17:22:30 +0000

    assignment 22 completed, menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,7 +14,8 @@
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View entry n"
+     puts "6 - Exit"
      print "Enter your selection: "
  
  # #3
@@ -37,6 +38,10 @@
        read_csv
        main_menu
      when 5
+       system "clear"
+       entry_n_submenu
+       main_menu
+     when 6  
        puts "Good-bye!"
  # #8
        exit(0)
@@ -48,6 +53,16 @@
      end
    end
  
+   def entry_n_submenu
+       print "Entry number to view: "
+       selection = gets.chomp.to_i
+       
+       if selection < @address_book.entries.count
+           puts @address_book.entries[selection]
+           gets.chomp
+           system "clear"
+       end
+   end   
  # #10
    def view_all_entries
      @address_book.entries.each do |entry|
