4ffb68045990a17ae4b650e9a53decdd437f1666
teckjon <demospata@hotmail.com>
2015-10-12 16:50:12 +0000

    Assignment 21 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,17 +7,26 @@
      @entries = []
    end
    
-   def add_entry(name, phone_number, email)
- # #9
-     index = 0
+   def remove_entry(name, phone_number, email)
+     delete_entry = nil
+     
      @entries.each do |entry|
- # #10
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+       delete_entry = entry
+      end 
+   end 
+     @entries.delete(delete_entry)   
+   end
+   
+   def add_entry(name, phone_number, email)
+     index = 0
+     @entries.each do |entry|0
        if name < entry.name
          break
        end
-       index += 1
+      index += 1
      end
- # #11
      @entries.insert(index, Entry.new(name, phone_number, email))
-   end   
+   end 
  end
+
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@
      end   
    end
   
+   describe "#remove_entry" do
+     it "removes an entry using the name, phone_number, and email address" do
+       book = AddressBook.new
+       book.add_entry("Micheal Faraday", "010.210.5181", "michael.faraday@faraday.com")
+       
+       name = "Ada Lovelace"
+       phone_number = "010.012.1815"
+       email_address = "augusta.king@lovelace.com"
+       book.add_entry(name, phone_number, email_address)
+       
+       expect(book.entries.size).to eq 2
+       book.remove_entry(name, phone_number, email_address)
+       expect(book.entries.size).to eq 1
+       expect(book.entries.first.name).to eq("Michael Faraday")
+     end  
+   end
+   
    describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
